57848eefc0c36ea098fac97a4d40342b9946e599
Chuyun Huang <chuyunhuang@pursuit.org>
2019-01-02 21:23:43 -0500

    implemented fs to create new file

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
+const adminService = require('./service/admin')
+const fs = require('fs')
+
 const express = require('express')
 
 let app = express()
 
 const port = 6500
 
-app.get('/', (req, res)=>{
-  res.send('Testing')
+// Adding student to a class
+app.get('/class/add', (req, res)=>{
+// creating file
+  let className = req.query.class
+  let dataToWrite = JSON.stringify(req.query)
+  fs.writeFile(`./classes/${className}.json`, dataToWrite, (err)=>{
+
+  })
+  res.send({
+    added: {name:req.query.name, age:req.query.age, city:req.query.city, grade: req.query.grade},
+    class: req.query.class
+  })
 });
 
+app.get('/class/list', (req, res)=>{
+  let className =req.query.class
+  
+  fs.readFile(`./classes/${className}.json`, (err, data)=>{
+    res.send(JSON.parse(data))
+  })
+})
+// Listing student to a class
 
+// List failing student
+
+// List student from a city
 
 
 
